fd493d123bf3a546ad5e0f83e3c359be367d2e2c
bayhax <whlbayhax@163.com>
2020-07-09 21:40:24 +0800

    页面美化

--- apps/ugc_home/static/js/create_server.js
+++ apps/ugc_home/static/js/create_server.js
@@ -1,23 +1,53 @@
+// 租赁服务器
+// 服务器是否对外开放
+$(document).on("change", "input[name='is_private']", function(){
+    if($("input[name='is_private']:checked").val() == 'on'){
+
+    }else{
+
+    }
+})
 // 服务器租赁价格
-function price() {
+function price(way) {
     var rent_time = $("input[name='rent_time']:checked").val();
     var number = $("input[name='package']:checked").val();
     if(rent_time == null || rent_time == 'on' || number == null){
         return false
     }else{
-        document.getElementById("score_purchase").innerText = number * rent_time + " 积分获得"
-        document.getElementById("money_purchase").innerText = number * rent_time + "￥ 购买"
+        if(way=="weChat"){
+            document.getElementById("amount").innerHTML = "<span class='weChat_pay_text'>微信扫码支付<span id='money'>" +
+                                                           number * rent_time+ "</span>元</span>"
+        }else{
+            document.getElementById("amount").innerHTML = "积分支付<span id='money'>" + number * rent_time + "</span>"
+        }
     }
-}
+};
+// 支付
+$("#score_pay").on("click", function(){
+    $("#weChat_pay").removeClass("pay_activate_li")
+    $("#score_pay").addClass("pay_activate_li")
+    $("#pay_way_hidden").val("2")
+    price("score")
+})
+$("#weChat_pay").on("click", function(){
+    $("#score_pay").removeClass("pay_activate_li")
+    $("#weChat_pay").addClass("pay_activate_li")
+    $("#pay_way_hidden").val("1")
+    price("weChat")
+})
 // 服务器租赁截止日期
 $(".rent_time_radio").change(function(){
+    // 日期对象
     var date = new Date(Date.now());
+    // 起始日期
+    var start = date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日';
     var rent_time = $("input[name='rent_time']:checked").val();
+    // 计算结束日期
     if(rent_time != 'on'){
         date.setTime(date.getTime() + rent_time * 60 * 60 * 24 * 1000);
-        var end = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-        document.getElementById("deadline").innerText = end;
-        price();
+        var end = date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日';
+        document.getElementById("deadline").innerText = start + '——' + end;
+        price("weChat");
     }
 })
 $(".package_radio").change(function(){
@@ -51,11 +81,12 @@ function pay_way(purchase_url){
         }
     }
 }
-// 积分获得
-$("#score_purchase").click(function(){
-    pay_way("/ugc_server/score_purchase")
-})
-// 人民币玩家
-$("#money_purchase").click(function(){
-    pay_way("/ugc_server/money_purchase")
+// 支付按钮
+$("#pay_button").on("click", function(){
+    var pay_way_hidden = $("#pay_way_hidden").val()
+    if(pay_way_hidden == "1"){
+        pay_way("/ugc_server/money_purchase")
+    }else{
+        pay_way("/ugc_server/score_purchase")
+    }
 })
--- apps/ugc_home/static/js/ugc_server.js
+++ apps/ugc_home/static/js/ugc_server.js
@@ -1,7 +1,7 @@
 // 服务器状态
 $(document).on("change", "input[name='server_status']", function(){
     if($("input[name='server_status']:checked").val() == 'on'){
-        this.previousElementSibling.innerText = "开放"
+        this.previousElementSibling.innerText = "开启"
     }else{
         this.previousElementSibling.innerText = "关闭"
     }
@@ -12,7 +12,7 @@ $(document).on("change", "input[name='is_server_private']", function(){
         //兄弟节点span元素改变值
         this.previousElementSibling.innerText = "开放"
     }else{
-        this.previousElementSibling.innerText = "关闭"
+        this.previousElementSibling.innerText = "私密"
     }
 })
 $("#ugc_server").addClass("active_button")
@@ -26,18 +26,26 @@ $.post('/ugc_server/ugc_server', function(server_data){
     server_manage.innerHTML = '';
     for(i=0;i<server_data.server_id.length;i++){
         // 开关1
+        var on_text = document.createElement("span")
+        on_text.className = "switch_on"
+        on_text.innerText = 'ON'
+
+        var off_text = document.createElement("span")
+        off_text.className = "switch_off"
+        off_text.innerText = 'OFF'
+
         var switch_inner = document.createElement('div');
         switch_inner.className = "switch_inner";
+
         var switch_box = document.createElement('div');
         switch_box.className = "switch_box";
-        switch_box.appendChild(switch_inner);
+        switch_box.append(switch_inner, on_text, off_text);
 
         var server_status = document.createElement('input')
         server_status.type = "checkbox"
         server_status.name = "server_status"
         server_status.id = "server_status_" + i
         server_status.hidden = "hidden"
-        //server_status.addEventListener('change', server_is_open())
 
         var server_status_text = document.createElement('span')
         server_status_text.class = "server_status_text"
@@ -50,11 +58,19 @@ $.post('/ugc_server/ugc_server', function(server_data){
         label.append(server_status_text, server_status, switch_box)
 
         // 开关2
+        var on_text2 = document.createElement("span")
+        on_text2.className = "switch_on"
+        on_text2.innerText = 'ON'
+
+        var off_text2 = document.createElement("span")
+        off_text2.className = "switch_off"
+        off_text2.innerText = 'OFF'
         var switch_inner2 = document.createElement('div')
         switch_inner2.className = "switch_inner"
+
         var switch_box2 = document.createElement('div')
         switch_box2.className = "switch_box"
-        switch_box2.appendChild(switch_inner2)
+        switch_box2.append(switch_inner2, on_text2, off_text2)
 
         var server_private = document.createElement('input')
         server_private.type = "checkbox"
@@ -78,27 +94,56 @@ $.post('/ugc_server/ugc_server', function(server_data){
         switch_show.appendChild(label2)
 
         // 服务器信息
-        var server_id = document.createElement('span')
+        var info_text = document.createElement('span')
+        info_text.className = "info_text"
+        info_text.innerText = "基本信息"
+
+        var server_id = document.createElement('div')
         server_id.className = "server_id"
-        server_id.innerText = "ID:" + server_data.server_id[i]
-        var server_name = document.createElement('span')
+        server_id.innerText = "ID: " + server_data.server_id[i]
+
+        var basic_info = document.createElement('div')
+        basic_info.className = "basic_info"
+        basic_info.append(info_text, server_id)
+
+        var hr = document.createElement('hr')
+        hr.className = "server_manage_hr"
+
+        var server_name = document.createElement('div')
         server_name.className = "server_name"
-        server_name.innerText = "名称：" + server_data.server_name[i]
-        var server_max_player = document.createElement('span')
+        server_name.innerText = "名称: " + server_data.server_name[i]
+
+        var server_max_player = document.createElement('div')
         server_max_player.className = "server_max_player"
-        server_max_player.innerText = "人数：3/" + server_data.server_max_player[i]
-        var server_rent_time = document.createElement('span')
+        server_max_player.innerText = "人数: 3/" + server_data.server_max_player[i]
+
+        var server_rent_time = document.createElement('div')
         server_rent_time.className = "server_rent_time"
-        server_rent_time.innerText = "有效期：" + server_data.server_start_time[i] + "至" + server_data.server_end_time[i]
-        var server_mod = document.createElement('span')
+        server_rent_time.innerText = "有效期: " + server_data.server_start_time[i] + " —— " + server_data.server_end_time[i]
+
+        var server_mod = document.createElement('div')
         server_mod.className = "server_mod"
-        server_mod.innerText = ""
+        server_mod.innerText = "MOD: 无"
 
         var server_info = document.createElement('div')
         server_info.className = "server_info"
-        server_info.append(server_id, server_name, server_max_player, server_rent_time, server_mod)
+        server_info.append(basic_info, hr, server_name, server_max_player, server_rent_time, server_mod)
 
+        // 服务器操作
+        var server_operate_text = document.createElement('span')
+        server_operate_text.className = "server_operate_text"
+        server_operate_text.innerText = "服务器操作"
+
+        var server_operate = document.createElement('div')
+        server_operate.className = "server_operate"
+        server_operate.appendChild(server_operate_text)
+
+        var hr2 = document.createElement('hr')
+        hr2.className = "server_manage_hr"
         // 服务器管理按钮
+        var change_password = document.createElement('button')
+        change_password.type = "button"
+        change_password.innerHTML = '修改密码'
         var renewal = document.createElement('button')
         renewal.type = "button"
         renewal.innerHTML = '续费'
@@ -109,14 +154,12 @@ $.post('/ugc_server/ugc_server', function(server_data){
         var server_manage_button = document.createElement('div')
         server_manage_button.className = "server_manage_button"
 
-        server_manage_button.appendChild(renewal)
-        server_manage_button.appendChild(edit_mod)
+        server_manage_button.append(change_password, renewal, edit_mod)
 
         // 加入服务器管理块
         var box = document.createElement('div')
         box.className = "server_manage_box"
-        box.innerText = "基本信息"
-        box.append(server_info, switch_show, server_manage_button)
+        box.append(server_info, server_operate, hr2, switch_show, server_manage_button)
 
         server_manage.appendChild(box)
     }
